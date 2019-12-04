2be7621a09fe0bc6471ce604229f276bb6323e3c
Jen Merritt <jenjomez@gmail.com>
2019-12-04 15:56:24 +0000

    view all artists and albums

--- db/console.rb
+++ db/console.rb
@@ -2,8 +2,9 @@ require('pry')
 require_relative('../models/album')
 require_relative('../models/artist')
 
-Artist.delete_all()
 Album.delete_all()
+Artist.delete_all()
+
 
 artist1 = Artist.new({
   'name' => "Whitney Houston"

05c0dcd813e6697889307e90bf35f2998b65d7f3
Jen Merritt <jenjomez@gmail.com>
2019-12-04 15:36:34 +0000

    setup sql file and classes

---
+++ models/album.rb
@@ -0,0 +1,16 @@
+require('pg')
+require_relative('../db/sql_runner')
+
+class Album
+
+  attr_reader :id
+  attr_accessor :title, :genre, :artist_id
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @title = options['title']
+    @genre = options['genre']
+    @artist_id = options['artist_id'].to_i
+  end
+
+end
